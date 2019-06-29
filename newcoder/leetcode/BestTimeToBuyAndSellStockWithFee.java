87b53c6c944c289308e2151d8635846b8a5e5705
zc-zangchao <49869493+zc-zangchao@users.noreply.github.com>
2019-06-29 09:20:13 +0800

    买卖股票的最佳时机含手续费 -- leetcode 714
    买卖股票的最佳时机含手续费 -- leetcode 714
    考点：贪心 动态规划

---
+++ newcoder/leetcode/BestTimeToBuyAndSellStockWithFee.java
@@ -0,0 +1,64 @@
+package com.my.test.leetcode.array;
+
+/**
+ * 题目：
+ * 买卖股票的最佳时机含手续费 -- leetcode 714
+ * 
+ * 题目描述：
+给定一个整数数组 prices，其中第 i 个元素代表了第 i 天的股票价格 ；非负整数 fee 代表了交易股票的手续费用。
+
+你可以无限次地完成交易，但是你每次交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
+
+返回获得利润的最大值。
+
+示例 1:
+
+输入: prices = [1, 3, 2, 8, 4, 9], fee = 2
+输出: 8
+解释: 能够达到的最大利润:  
+在此处买入 prices[0] = 1
+在此处卖出 prices[3] = 8
+在此处买入 prices[4] = 4
+在此处卖出 prices[5] = 9
+总利润: ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
+注意:
+
+0 < prices.length <= 50000.
+0 < prices[i] < 50000.
+0 <= fee < 50000.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ */
+public class BestTimeToBuyAndSellStockWithFee
+{
+    
+    /**
+     * 思路：贪心算法
+     */
+    public static int maxProfit(int[] prices, int fee) {
+        if (prices == null || prices.length <= 0 || fee < 0) {
+            return 0;
+        }
+        
+        // buy代表当天买股票的最大收益，sell代表当天卖出股票的最大收益
+        int buy = Integer.MIN_VALUE;
+        int sell = 0;
+        
+        for (int p : prices) {
+            buy = Math.max(buy, sell - p - fee);
+            sell = Math.max(sell, buy + p);
+        }
+        
+        return sell;
+    }
+    
+    public static void main(String[] args)
+    {
+        int[] prices = {1,3,7,5,10,3};
+        System.out.println(maxProfit(prices, 3));
+    }
+
+}
