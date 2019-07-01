d8ff820e2d0a741eb54f46958f896fe6afcc748f
zc-zangchao <49869493+zc-zangchao@users.noreply.github.com>
2019-07-01 10:41:23 +0800

    打家劫舍III -- leetcode 337
    打家劫舍III -- leetcode 337
    考点：DFS

---
+++ newcoder/leetcode/HouseRobberIII.java
@@ -0,0 +1,88 @@
+package com.my.test.leetcode.dyncplanning;
+
+/**
+ * 题目：
+ * 打家劫舍III -- leetcode 337
+ * house robber iii
+ * 
+ * 题目描述：
+ * 
+在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+
+计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+
+示例 1:
+
+输入: [3,2,3,null,3,null,1]
+
+     3
+    / \
+   2   3
+    \   \ 
+     3   1
+
+输出: 7 
+解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+示例 2:
+
+输入: [3,4,5,1,3,null,1]
+
+     3
+    / \
+   4   5
+  / \   \ 
+ 1   3   1
+
+输出: 9
+解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/house-robber-iii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class HouseRobberIII
+{
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    } 
+    
+    /**
+     * 思路：
+     * 1、使用一个二元素数组res记录抢到的金额
+     * 2、res[0],res[1]分别代表不包含根节点与包含根节点的金额
+     * 3、相比取最大值
+     */
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int[] res = helpRob(root); 
+                        
+        return Math.max(res[0], res[1]);
+    }
+    
+    private int[] helpRob(TreeNode node) {
+        int[] res = new int[2];
+        if (node == null) {
+            return res;
+        }
+        
+        // 左子树 不包含根节点与包含根节点 值
+        int[] left = helpRob(node.left);
+        int[] right = helpRob(node.right);
+        
+        // 不包含本节点
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        
+        // 包含本节点
+        res[1] = left[0] + right[0] + node.val;
+        
+        return res;
+    }
+    
+    
+}
